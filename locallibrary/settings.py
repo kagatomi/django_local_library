f2a256173a36d48a2b8ae685a2d3a5dc72c8d0a0
kagatomi <thientinlk@gmail.com>
2017-11-17 18:08:08 +0700

    Update settings.py

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -20,10 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1v^5pa_qb(kec9&t5&berwzl5b(v9!qk1cz91cm7k-mi887kl@'
+# SECRET_KEY = '1v^5pa_qb(kec9&t5&berwzl5b(v9!qk1cz91cm7k-mi887kl@'
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '1v^5pa_qb(kec9&t5&berwzl5b(v9!qk1cz91cm7k-mi887kl@')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+# DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
 
